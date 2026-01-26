72cfca37c086d3677254306508c3659ffa21f347
surferviki <37582083+surferviki@users.noreply.github.com>
2026-01-26 16:09:07 +0000

    Remove container alive comment from main.py
    Removed the comment about keeping the container alive for 60 seconds.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
 from story_generator import generate_story, save_story, send_whatsapp
+import time
 
 def main():
     story = generate_story()
@@ -7,10 +7,8 @@ def main():
     print(story)
 
     save_story(story)
-
     send_whatsapp(story)
 
-    # Keep the container alive 60 seconds so we can see logs
     print("\n⏳ Keeping container alive for 60 seconds...")
     time.sleep(60)
 
